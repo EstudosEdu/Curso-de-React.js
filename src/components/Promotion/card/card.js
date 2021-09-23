231d472f3638fa8319936c464724a74194ce092d
Junior042 <eduardoalvesdeoliveirajunior@gmail.com>
2021-09-22 22:28:45 -0300

    fix botões

--- src/components/Promotion/card/card.js
+++ src/components/Promotion/card/card.js
@@ -11,6 +11,7 @@ const PromotionCard = ({ promotion }) => (
     <div className="promotion-card__info">
       <h1 className="promotion-card__title">{promotion.title}</h1>
       <span className="promotion-card__price">R$ {promotion.price}</span>
+      
       <footer className="promotion-card__footer">
         {promotion.comments.length > 0 && (
           <div className="promotion-card__comment">
